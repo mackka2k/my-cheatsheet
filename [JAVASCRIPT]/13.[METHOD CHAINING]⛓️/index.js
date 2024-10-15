e495f07684ebd0d3ff685f57870e3ef8946083cf
Evaldas Mackonis <118393420+mackka2k@users.noreply.github.com>
2024-10-15 19:07:17 +0300

    Delete [JAVASCRIPT] directory

--- [JAVASCRIPT]/13.[METHOD CHAINING]⛓️/index.js
+++
@@ -1,8 +0,0 @@
-// method chaining = calling  one method after another
-//                   in one continuous line of code
-
-let userName = 'bro'
-
-let letter = userName.charAt(0).toUpperCase().trim()
-
-console.log(letter) // B
--- [JAVASCRIPT]/34.[SORT AN ARRAY OF STRINGS]🗃️/index.js
+++
@@ -1,8 +0,0 @@
-let fruits = ['apple', 'banana', 'kiwi', 'orange', 'strawberry']
-
-// fruits = fruits.sort() // sort the array
-fruits = fruits.sort.applyreverse() // sort and reverse the array
-
-for (let fruit of fruits) {
-	console.log(fruit)
-}
