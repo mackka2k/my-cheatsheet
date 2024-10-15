c50b427e4ec6c883872271392e1328ecd1a0e259
Evaldas Mackonis <118393420+mackka2k@users.noreply.github.com>
2024-10-15 19:07:59 +0300

    Delete [REACT] directory

--- [REACT]/31.[TANSTACK-QUERY]/src/types/product.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IProduct {
-	id: number
-	name: string
-	price: string
-	image: string
-}
-
-export interface IProductData extends Omit<IProduct, 'id'> {}
--- [REACT]/7.[CLICK-EVENTS]/src/ProfilePicture.jsx
+++
@@ -1,9 +0,0 @@
-function ProfilePicture() {
-	const imageUrl = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50';
-
-	const handleClick = (e) => e.target.style.display = "none";
-
-	return (<img onClick={(e) => handleClick(e)} src={imageUrl} alt="Profile" />)
-}
-
-export default ProfilePicture;
