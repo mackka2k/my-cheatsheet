5feb1803f9a2c7889bdb0116982d2a15cd7fd7e3
Evaldas Mackonis <118393420+mackka2k@users.noreply.github.com>
2024-10-15 19:08:07 +0300

    Delete [TYPESCRIPT] directory

--- [TYPESCRIPT]/10.[ASSERTIONS]/main.ts
+++
@@ -1,5 +0,0 @@
-// assertions is when we dont know the type of a variable and we want to tell the compiler what type it is
-
-const inputElement = document.querySelector('input')
-const value1 = (inputElement as HTMLInputElement).value
-const value2 = (<HTMLInputElement>inputElement).value
