84c4bd6c8b849232ae652f9194c67b14c0245342
Evaldas Mackonis <118393420+mackka2k@users.noreply.github.com>
2024-10-15 19:07:49 +0300

    Delete [PHP] directory

--- [PHP]/13.[RADIO BUTTONS]/process_form.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve the selected gender value
-    $selectedGender = $_POST['gender'];
-
-    // Display the selected gender
-    echo "You selected: $selectedGender";
-}
